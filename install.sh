a87ac2350b13cf6de38def015e810212801e07ea
Daniele Isoni <daniele.isoni@studenti.unitn.it>
2022-02-01 22:49:02 +0000

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
-export USERNAME=`daniso`
+export USERNAME=$(whoami)
 
-is_linux() { [ $(uname -s) -eq "Linux" ]; }
-is_macos() { [ $(uname -s) -eq "Darwin" ]; }
+is_linux() { [ $(uname -s) == "Linux" ]; }
+is_macos() { [ $(uname -s) == "Darwin" ]; }
+
+if $CODESPACES; then
+    DOTFILES_DIR="/workspaces/.codespaces/.persistedshare/dotfiles"
+else
+    DOTFILES_DIR="$HOME/dotfiles"
+fi
 
 mkdir dev
 mkdir .virtualenvs
@@ -12,37 +18,50 @@ then
     sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
 
     sudo apt-get install -y \
-    zsh \
-    snapd;
+    zsh;
 else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.tool-versions ~/.tool-versions
-if is_linux; then chsh -s /usr/bin/zsh $USERNAME; fi
+ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
+ln -sf $DOTFILES_DIR/.tool-versions ~/.tool-versions
+ln -sf $DOTFILES_DIR/.p10k.zsh ~/.p10k.zsh
+if is_linux; then sudo chsh -s /usr/bin/zsh $USERNAME; fi
 
 # oh-my-zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# oh-my-zsh powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# install fonts
+mkdir -p ~/.local/share/fonts 
+wget -O ~/.local/share/fonts/MesloLGS\ NF\ Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+wget -O ~/.local/share/fonts/MesloLGS\ NF\ Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget -O ~/.local/share/fonts/MesloLGS\ NF\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+wget -O ~/.local/share/fonts/MesloLGS\ NF\ Bold\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+fc-cache -f -v
 
 # oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-ln -s ~/dotfiles/oh-my-zsh/custom/plugins/git-auto-status ~/.oh-my-zsh/custom/plugins/git-auto-status
+ln -sf $DOTFILES_DIR/oh-my-zsh/custom/plugins/git-auto-status ~/.oh-my-zsh/custom/plugins/git-auto-status
 
 # install some things
 if is_linux;
 then
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf;
     sudo apt-get install -y fzf;
-    apt-get install git-flow;
+    sudo apt-get install git-flow;
 else
     brew install asdf;
     brew install fzf;
     brew install git-flow-avh;
     brew install micro;
 fi
+
+. $HOME/.asdf/asdf.sh;
 
 asdf plugin add python
 asdf install python 3.7.12
@@ -59,4 +78,5 @@ asdf install nodejs 16.8.0
 asdf plugin add poetry
 asdf install poetry 1.1.12
 
+pip install --upgrade pip
 pip install virtualenvwrapper
