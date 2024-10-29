deec64a81483f0a9e18acf4b01e054c233e50092
DanieleIsoni <daniele.isoni@icloud.com>
2024-10-29 23:11:21 +0100

    nvim: change some icons

--- config/nvim/lua/daniso/plugins/lualine.lua
+++ config/nvim/lua/daniso/plugins/lualine.lua
@@ -28,7 +28,18 @@ return {
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_b = {
+          'branch',
+          {
+            'diff',
+            symbols = {
+              added = ' ',
+              modified = ' ',
+              removed = ' ',
+            },
+          },
+          'diagnostics',
+        },
         lualine_c = {
           { 'filetype', icon_only = true, separator = '', padding = { left = 1, right = 0 } },
           { 'filename', path = 1 },
