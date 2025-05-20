244cc9eca9fcadfc23d3dc121d9e90192b90f620
DanieleIsoni <daniele.isoni@icloud.com>
2025-05-20 12:25:40 +0200

    nvim: avante: blink provider

--- config/nvim/lua/daniso/plugins/coding-assistants.lua
+++ config/nvim/lua/daniso/plugins/coding-assistants.lua
@@ -94,6 +94,28 @@ return {
       },
     },
   },
+  {
+    'saghen/blink.cmp',
+    dependencies = {
+      'Kaiser-Yang/blink-cmp-avante',
+    },
+    opts = {
+      sources = {
+        -- Add 'avante' to the list
+        default = { 'avante' },
+        providers = {
+          avante = {
+            module = 'blink-cmp-avante',
+            name = 'Avante',
+            opts = {
+              -- options for blink-cmp-avante
+            },
+          },
+        },
+      },
+    },
+  },
+
   -- {
   --   'milanglacier/minuet-ai.nvim',
   --   dependencies = {
