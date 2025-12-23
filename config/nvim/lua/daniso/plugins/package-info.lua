8bc4d83e9dcb71c0d434e1f5c3df1889abd7330d
DanieleIsoni <daniele.isoni@icloud.com>
2025-12-23 16:55:36 +0100

    nvim: comment some plugins

--- config/nvim/lua/daniso/plugins/package-info.lua
+++ config/nvim/lua/daniso/plugins/package-info.lua
@@ -6,25 +6,25 @@ require('which-key').add {
   { '<leader>pt', group = '[P]ackage info: [T]s', icon = '' },
 }
 return {
-  {
-    'DanieleIsoni/py-package-info.nvim',
-    -- dir = "~/dev/personal/py-package-info.nvim/",
-    dependencies = { 'MunifTanjim/nui.nvim' },
-    config = function()
-      require('py-package-info').setup()
-
-      require('which-key').add {
-        { '<leader>pp', group = '[P]ackage info: [P]y' },
-      }
-
-      -- stylua: ignore start
-      vim.keymap.set('n', '<leader>ppt', ":lua require('py-package-info').toggle({ force = true })<CR>", { desc = '[P]ackage info: [P]y [T]oggle Info' })
-      vim.keymap.set('n', '<leader>ppi', ":lua require('py-package-info').install()<CR>", { desc = '[P]ackage info: [P]y [I]nstall package' })
-      vim.keymap.set('n', '<leader>ppu', ":lua require('py-package-info').update()<CR>", { desc = '[P]ackage info: [P]y [U]pdate package' })
-      vim.keymap.set('n', '<leader>ppd', ":lua require('py-package-info').delete()<CR>", { desc = '[P]ackage info: [P]y [D]elete package' })
-      -- stylua: ignore end
-    end,
-  },
+  -- {
+  --   'DanieleIsoni/py-package-info.nvim',
+  --   -- dir = "~/dev/personal/py-package-info.nvim/",
+  --   dependencies = { 'MunifTanjim/nui.nvim' },
+  --   config = function()
+  --     require('py-package-info').setup()
+  --
+  --     require('which-key').add {
+  --       { '<leader>pp', group = '[P]ackage info: [P]y' },
+  --     }
+  --
+  --     -- stylua: ignore start
+  --     vim.keymap.set('n', '<leader>ppt', ":lua require('py-package-info').toggle({ force = true })<CR>", { desc = '[P]ackage info: [P]y [T]oggle Info' })
+  --     vim.keymap.set('n', '<leader>ppi', ":lua require('py-package-info').install()<CR>", { desc = '[P]ackage info: [P]y [I]nstall package' })
+  --     vim.keymap.set('n', '<leader>ppu', ":lua require('py-package-info').update()<CR>", { desc = '[P]ackage info: [P]y [U]pdate package' })
+  --     vim.keymap.set('n', '<leader>ppd', ":lua require('py-package-info').delete()<CR>", { desc = '[P]ackage info: [P]y [D]elete package' })
+  --     -- stylua: ignore end
+  --   end,
+  -- },
   {
     'vuki656/package-info.nvim',
     dependencies = { 'MunifTanjim/nui.nvim' },
@@ -45,9 +45,9 @@ return {
     optional = true,
     event = 'VeryLazy',
     opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, 3, function()
-        return require('py-package-info').get_status()
-      end)
+      -- table.insert(opts.sections.lualine_x, 3, function()
+      --   return require('py-package-info').get_status()
+      -- end)
       table.insert(opts.sections.lualine_x, 4, function()
         return require('package-info').get_status()
       end)
--- config/nvim/lua/daniso/plugins/typescript.lua
+++ config/nvim/lua/daniso/plugins/typescript.lua
@@ -1,19 +1,19 @@
 -- Typescript
 
 return {
-  {
-    'nvim-neotest/neotest',
-    dependencies = { 'haydenmeade/neotest-jest' },
-    opts = function(_, opts)
-      table.insert(
-        opts.adapters,
-        require 'neotest-jest' {
-          jestCommand = 'npx jest',
-          cwd = function()
-            return vim.fn.getcwd()
-          end,
-        }
-      )
-    end,
-  },
+  -- {
+  --   'nvim-neotest/neotest',
+  --   dependencies = { 'haydenmeade/neotest-jest' },
+  --   opts = function(_, opts)
+  --     table.insert(
+  --       opts.adapters,
+  --       require 'neotest-jest' {
+  --         jestCommand = 'npx jest',
+  --         cwd = function()
+  --           return vim.fn.getcwd()
+  --         end,
+  --       }
+  --     )
+  --   end,
+  -- },
 }
